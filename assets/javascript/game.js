f9106b3b44e2cee09aaeddbc43551b45d87c8ed5
Simmigon <simmigon.flagg@gmail.com>
2018-07-23 09:15:06 -0400

    Final push. I thinking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,26 +12,24 @@ $(document).ready(function () {
             $("#my_guesses").text("Your guesses so far: " + my_guesses);
             $("#remaining_guesses").text("Guesses Left: " + remaining_guessesCounter);
 
-            console.log(remaining_guessesCounter);
         } else {
 
             if (random_comuter_choice === e.keyCode) {
                 wins++;
                 $("#wins").text("Wins: " + wins);
                 random_comuter_choice = Math.floor((Math.random() * 25) + 65);
-                $("#test").text("Computer's random letter:" + random_comuter_choice);
+               
             } else {
                 losts++;
                 $("#losses").text("Losses: " + losts);
                 random_comuter_choice = Math.floor((Math.random() * 25) + 65);
-                $("#test").text("Computer's random letter:" + random_comuter_choice);
+
             }
 
             my_guesses = []
             remaining_guessesCounter = 9;
             $("#my_guesses").text("Your guesses so far: " + my_guesses);
             $("#remaining_guesses").text("Guesses Left: " + remaining_guessesCounter);
-
         }
     });
 
